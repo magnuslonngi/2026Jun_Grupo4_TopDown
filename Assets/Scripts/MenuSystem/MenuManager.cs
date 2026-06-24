71016cd0e30b951cb691cfd454adb77638a7f14d
Magnus Lonngi <ethanlonngi@gmail.com>
2026-06-24 08:06:59 -0600

    Added lose and win condition

--- Assets/Scripts/MenuSystem/MenuManager.cs
+++ Assets/Scripts/MenuSystem/MenuManager.cs
@@ -5,10 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [Header("Player")]
+    [SerializeField] private GameObject _player;
+
     [Header("Pause Options")]
     [SerializeField] private InputActionReference _pauseInput;
     [SerializeField] private GameObject _pauseMenu;
+
     public bool CanPause = false;
+
+    [Header("Win and Lose References")]
+    [SerializeField] private GameObject _winMenu;
+    [SerializeField] private GameObject _loseMenu;
 
     [HideInInspector] public bool IsGamePaused = false;
 
@@ -16,18 +24,40 @@ public class MenuManager : MonoBehaviour
     public UnityEvent OnGamePause;
     public UnityEvent OnGameResume;
 
+    private Health _playerHealth;
+
+    private void Awake()
+    {
+        if (_player != null)
+        {
+            _playerHealth = _player.GetComponent<Health>();
+        }
+    } 
+
     private void OnEnable()
     {
-        if (_pauseInput == null) return;
+        if (_pauseInput != null)
+        {
+            _pauseInput.action.performed += TogglePause;
+        }
 
-        _pauseInput.action.performed += TogglePause;
+        if (_player != null)
+        {
+            _playerHealth.OnHealthDeplete.AddListener(PlayerDead);
+        }
     }
     
     private void OnDisable()
     {
-        if (_pauseInput == null) return;
+        if (_pauseInput != null)
+        {
+            _pauseInput.action.performed -= TogglePause;
+        }
 
-        _pauseInput.action.performed -= TogglePause;
+        if (_player != null)
+        {
+            _playerHealth.OnHealthDeplete.RemoveListener(PlayerDead);
+        }
     }
 
     public void GoToScene(int sceneNumber)
@@ -43,6 +73,29 @@ public class MenuManager : MonoBehaviour
     public void DisableItem(GameObject item)
     {
         item.SetActive(false);
+    }
+
+    private void PlayerDead(float health)
+    {
+        ShowLoseMenu();
+    }
+
+    private void ShowWinMenu()
+    {
+        _winMenu?.SetActive(true);
+
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        IsGamePaused = true;
+    }
+
+    private void ShowLoseMenu()
+    {
+        _loseMenu?.SetActive(true);
+
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        IsGamePaused = true;
     }
 
     private void TogglePause(InputAction.CallbackContext context)
